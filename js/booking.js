7b9e7b9b2f6c3179596adea1ffd6ffb836239596
John Powell <powell.john2011@gmail.com>
2022-03-25 09:17:35 +0300

    adding booking display

--- js/booking.js
+++ js/booking.js
@@ -16,16 +16,16 @@ $(document).ready(function(){
         e.preventDefault();
         let firstName = $("#firs-name").val();
         let lastName = $("#last-name").val();
-        let phone = $("#tel").val();
+        let phone = $("#tel").val()
         let email = $("#email").val();
         let destination = $("#destination").val();
-        let startDate = $("start-date").val();
-        let endDate = new Date($("end-date").val());
+        let startDate = $("#start-date").val();
+        let endDate = $("#end-date").val();
         const myBooking = new Booking(firstName,lastName,phone,email,destination,startDate,endDate);
-        console.log(startDate);
         let markup = `<tr><td>${myBooking.phone}</td><td>${myBooking.email}</td><td>${myBooking.destination}</td><td>${myBooking.startDate}</td><td>${myBooking.endDate}</td></tr>`
         console.log(markup);
         $("table tbody").append(markup);
+        $(".disp-booking").show();
         myBookings.push(myBooking);
     })
 
